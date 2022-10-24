18d8083328e76f10fd855d973463fd7fef64a1d6
Jonathas Rosa <jonathasrosa85@gmail.com>
2022-10-24 06:25:16 -0300

    Update Project Angular

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -22,7 +22,8 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input'
+import { MatInputModule } from '@angular/material/input';
+import { ProductReadComponent } from './components/product/product-read/product-read.component'
 
 
 @NgModule({
@@ -34,6 +35,7 @@ import { MatInputModule } from '@angular/material/input'
     HomeComponent,
     ProductCrudComponent,
     ProductCreatComponent,
+    ProductReadComponent,
   ],
   imports: [
     BrowserModule,
